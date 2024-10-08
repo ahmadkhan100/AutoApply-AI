48e9dec5f71f77d9641d10a4599f2f83d91e19f5
Ahmad Khan <163046609+ahmadkhan100@users.noreply.github.com>
2024-10-08 01:50:21 -0400

    Update test_autoapply_authenticator.py

--- tests/test_autoapply_authenticator.py
+++ tests/test_autoapply_authenticator.py
@@ -1,5 +1,5 @@
 import pytest
-from selenium.webdriver.common.by import Byy
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from src.aihawk_authenticator import AIHawkAuthenticator
